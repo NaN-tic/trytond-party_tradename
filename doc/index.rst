0af227519ede44dec0b6434965d58dd44d09fd8b
resteve <resteve@zikzakmedia.com>
2012-10-30 13:18:59 +0100

    Initial release

---
+++ doc/index.rst
@@ -0,0 +1,4 @@
+Party Trade Name Module
+#######################
+
+The party_tradename module add trade name field in accounting party tab.
---
+++ party.py
@@ -0,0 +1,13 @@
+#This file is part party_tradename module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['Party']
+__metaclass__ = PoolMeta
+
+class Party:
+    'Party'
+    __name__ = 'party.party'
+    tradename = fields.Char('Trade Name')
