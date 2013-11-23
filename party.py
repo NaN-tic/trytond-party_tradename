759236191d51ec520ba59aa8d873a8d9c448a6f2
Albert Cervera i Areny <albert@nan-tic.com>
2013-11-23 14:07:58 +0100

    Rename tradename to trade_name.

--- party.py
+++ party.py
@@ -3,6 +3,8 @@
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.transaction import Transaction
+from trytond import backend
 
 __all__ = ['Party']
 __metaclass__ = PoolMeta
@@ -10,4 +12,11 @@ __metaclass__ = PoolMeta
 
 class Party:
     __name__ = 'party.party'
-    tradename = fields.Char('Trade Name')
+    trade_name = fields.Char('Trade Name')
+
+    @classmethod
+    def __register__(cls, module_name):
+        cursor = Transaction().cursor
+        TableHandler = backend.get('TableHandler')
+        table = TableHandler(cursor, cls, module_name)
+        table.column_rename('tradename', 'trade_name')
