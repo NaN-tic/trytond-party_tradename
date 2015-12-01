20225fdfeee663ee0a62a86375b58eeb0d2c2e06
resteve <resteve@zikzakmedia.com>
2015-12-01 11:11:52 +0100

    Tests

--- tests/test_party_tradename.py
+++ tests/test_party_tradename.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part party_tradename module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the party_tradename module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyTradenameTestCase(unittest.TestCase):
-    'Test Party Trade Name module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_tradename')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_tradename')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class PartyTradenameTestCase(ModuleTestCase):
+    'Test Party Tradename module'
+    module = 'party_tradename'
 
 
 def suite():
