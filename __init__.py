eb7defc4808cc1cd74c125528959983ce5c3a6c0
C?dric Krier <ced@b2ck.com>
2018-01-23 11:27:56 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-#This file is part party_tradename module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part party_tradename module for Tryton.  The COPYRIGHT file at
+# the top level of this repository contains the full copyright notices and
+# license terms.
 from trytond.pool import Pool
-from .party import *
+from . import party
 
 
 def register():
     Pool.register(
-        Party,
+        party.Party,
         module='party_tradename', type_='model')
