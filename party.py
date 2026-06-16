e9f078de0cf665d75ef0045970d045df67f00b7d
Albert Cervera i Areny <albert@nan-tic.com>
2026-06-16 17:31:46 +0200

    Use __table_handler__ | #187981

--- party.py
+++ party.py
@@ -3,7 +3,6 @@
 # license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond import backend
 
 
 class Party(metaclass=PoolMeta):
@@ -13,7 +12,7 @@ class Party(metaclass=PoolMeta):
     @classmethod
     def __register__(cls, module_name):
         super(Party, cls).__register__(module_name)
-        table = backend.TableHandler(cls, module_name)
+        table = cls.__table_handler__(module_name)
         table.column_rename('tradename', 'trade_name')
 
     @classmethod
